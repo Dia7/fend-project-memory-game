48fa2c02824f9521d2e53c10115ad444ec3f00f2
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-03-29 15:48:04 +0300

    Timer(counter) & Moves /some rating adjustments/
     - declare variable for star icon
    
     - in startGame() fn. reset timer, moves and rating
    
     - in cardOpen() fn:
    
            - call fn. moveCounter()
    
            - setting rates, based on moves
    
            - set timer on first move
    
     - set the game timer
    
     - declare fn startTimer()

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ const deck = document.querySelector("#card-deck");
 let moves = 0;
 let counter = document.querySelector(".moves");
 
+// declare variables for star icons
+const stars = document.querySelectorAll(".fa-star");
+
 // * Open the cards on the page
 var displayCard = $('.card').on('click', function(event) {
     $(event.target).toggleClass('show');
@@ -29,15 +32,24 @@ document.body.onload = startGame();
 
 function startGame(){
     // *   - shuffle the list of cards using the provided "shuffle" method below
-   var shuffledCards = shuffle(cardList);
-   for (var i= 0; i < shuffledCards.length; i++){
-      [].forEach.call(shuffledCards, function(item){
-         deck.appendChild(item);
-      });
-   }
-   // shuffle deck
-     cardList = shuffle(cardList);
-     // remove all existing classes from each card
+    cardList = shuffle(cardList);
+
+    //reset timer
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+
+     // reset moves
+    moves = 0;
+    counter.innerHTML = moves;
+
+    // reset rating
+    for (var i= 0; i < stars.length; i++){
+        stars[i].style.color = "#FFD700";
+        stars[i].style.visibility = "visible";
+    }
+
+    // remove all existing classes from each card
     for (var i = 0; i < cardList.length; i++){
         deck.innerHTML = "";
         [].forEach.call(cardList, function(item) {
@@ -45,6 +57,8 @@ function startGame(){
         });
         cardList[i].classList.remove("show", "open", "match","disabled");
      }
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -61,10 +75,10 @@ function shuffle(array) {
 }
 // *  - add the card to a OpenedCards *list* of "open" cards to see if the cards are matching (put this functionality in another function that you call from this one)
 function cardOpen() {
+    moveCounter();
     openedCards.push(this);
     var len = openedCards.length;
     if(len === 2){
-        moveCounter();
         if(openedCards[0].type === openedCards[1].type){
             matched();
         } else {
@@ -121,12 +135,57 @@ function enable(){
 function moveCounter(){
     moves++;
     counter.innerHTML = moves;
+
+    //start timer on first move
+    if(moves === 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+       // setting rates based on moves
+    if (moves > 8 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
 }
+
  // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+
 
 // loop to add event listeners to each card
 for (var i = 0; i < cardList.length; i++){
     oneCard = cardList[i];
     oneCard.addEventListener("click", displayCard);
     oneCard.addEventListener("click", cardOpen);
+}
+
+// set the game timer
+var second = 0, minute = 0;
+var timer = document.querySelector(".timer");
+var interval;
+
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
 }
